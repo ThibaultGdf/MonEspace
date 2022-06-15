e71ac0bd0165d4b471ddeac001a15c2217e4ae33
ThibaultGdf <thibaultgodef81@gmail.com>
2022-06-15 15:26:03 +0200

    Message

---
+++ OnVaReussirUnJour/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnVaReussirUnJour
+//
+//  Created by Apprenant 20 on 15/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnVaReussirUnJour/OnVaReussirUnJourApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnVaReussirUnJourApp.swift
+//  OnVaReussirUnJour
+//
+//  Created by Apprenant 20 on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnVaReussirUnJourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
